3d8b30ffce46eb7917ad0ebf118934ab382e38f5
87RITU-KUSHWAH77 <ritikushwah877@gmail.com>
2025-07-09 23:46:53 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+function isAdult(){
+    let age = prompt("enter your age:")
+    if (age == 18){
+        console.log("You are Adult!")
+    }else{
+        console.log("you are not Adult!")
+    }
+}
