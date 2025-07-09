c682ed523cd7dd2ebaf75a2a52219a3e1678efb6
87RITU-KUSHWAH77 <ritikushwah877@gmail.com>
2025-07-09 23:38:42 +0530

    function

---
+++ index.java
@@ -0,0 +1,7 @@
+// //funcion are the reussable part of code
+// import java.util.*;
+// public class functin {
+//     // public static int calsum (int a, int b) {
+        
+//     //     int sum = a+b;
+//     //     return sum;
