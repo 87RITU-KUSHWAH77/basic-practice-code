e3336380e223d166e5113ce1a5ae31188c02273e
87RITU-KUSHWAH77 <ritikushwah877@gmail.com>
2025-07-09 23:50:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,3 +6,13 @@ function isAdult(){
         console.log("you are not Adult!")
     }
 }
+
+
+function isuser(){
+    let password = prompt("pls enter the valid password to enter");
+    if(password == correct){
+        console.log("login successfully");
+    }else{
+        console.log("pls enter the valiid password");
+    }
+}
