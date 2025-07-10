7e8ea0a79d4d133ba7ce504ea1dede8e559c79ad
87RITU-KUSHWAH77 <ritikushwah877@gmail.com>
2025-07-10 14:05:02 +0530

    delete all file

--- index.java
+++
@@ -1,7 +0,0 @@
-// //funcion are the reussable part of code
-// import java.util.*;
-// public class functin {
-//     // public static int calsum (int a, int b) {
-        
-//     //     int sum = a+b;
-//     //     return sum;
