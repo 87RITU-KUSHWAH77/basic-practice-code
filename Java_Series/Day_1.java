b96f2ded12a745e67612016ab776c54fab0b6ce8
87RITU-KUSHWAH77 <ritikushwah877@gmail.com>
2025-07-28 16:20:26 +0530

    Update Day_1.java

--- Java_Series/Day_1.java
+++ Java_Series/Day_1.java
@@ -38,50 +38,50 @@ public class Day_1{
         //     // Assignment Day-1 
 
         //     // 1
-        //     int age = 20;
-        //     float height = 5.6f;
-        //     Double salary = 1560000000000000000000000.00;
-        //     boolean is_working = true;
-        //     char gender = 'f';
+            int age = 20;
+            float height = 5.6f;
+            Double salary = 1560000000000000000000000.00;
+            boolean is_working = true;
+            char gender = 'f';
 
-        //     System.out.println("Age :" + age);
-        //     System.out.println("Height :"+ height);
-        //     System.out.println("salary :" + salary);
-        //     System.out.println("IsWorking :" + is_working);
-        //     System.out.println("Gender :" +gender);
+            System.out.println("Age :" + age);
+            System.out.println("Height :"+ height);
+            System.out.println("salary :" + salary);
+            System.out.println("IsWorking :" + is_working);
+            System.out.println("Gender :" +gender);
 
 
         //     // 2 swap with or without third var
-        //     int a = 5;
-        //     int b = 6;
-        //     System.out.println("a :" +a);
-        //     System.out.println("b :" +b);
+            int a = 5;
+            int b = 6;
+            System.out.println("a :" +a);
+            System.out.println("b :" +b);
             
-        //     int temp = a;         // temp var logic
-        //     a = b;
-        //     b = temp;
+            int temp = a;         // temp var logic
+            a = b;
+            b = temp;
 
-        //     a = a+b;  //  a -> 11
-        //     b = a-b;  //  b -> 11-6 = 5  with swap
-        //     a = a-b;  // a -> 11-5 = 6  with swap  
-        //     System.out.println("a :" +a);
-        //     System.out.println("b :" +b);
+            a = a+b;  //  a -> 11
+            b = a-b;  //  b -> 11-6 = 5  with swap
+            a = a-b;  // a -> 11-5 = 6  with swap  
+            System.out.println("a :" +a);
+            System.out.println("b :" +b);
 
 
         //    // 3 area of circle 
-        //     System.out.println("enter the radius:");
-        //     float r = Sc.nextFloat();
+            System.out.println("enter the radius:");
+            float r = Sc.nextFloat();
 
-        //     float area = Math.PI*r*r;
-        //     System.out.println("Area of a Circle :" + area);
+            float area = Math.PI*r*r;
+            System.out.println("Area of a Circle :" + area);
 
-        //     // 4 Salary double 
-        //     double salary = 2500000.0;
-        //     System.out.println("your updated Salary:" + salary*salary);
+            // 4 Salary double 
+            double salary = 2500000.0;
+            System.out.println("your updated Salary:" + salary*salary);
 
         //     // 5
-        //     char name  = 'A';
-        //     System.out.println(name+2);
+            char name  = 'A';
+            System.out.println(name+2);
 
 
 
